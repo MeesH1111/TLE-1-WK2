ae21d0cd287db4753b6ce364498c5a9c6e0b89e5
Irpear <145675108+Irpear@users.noreply.github.com>
2024-09-23 14:05:36 +0200

    .

---
+++ TLE1 WK2/doneren.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav> </nav>
+    <main>
+        <H1>Hier kunt u doneren aan Tuvalu</H1>
+
+        <form action="">
+
+            <label for="name">Uw naam</label>
+            <input type="text" id="name" name="name" placeholder="Naam" required>
+
+            <label for="donation">Uw donatie</label>
+            <input type="int" id="donation" name="donation" placeholder="€..." required>
+        </form>
+
+    </main>
+</body>
+
+</html>
