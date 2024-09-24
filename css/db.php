d493cd09a9022b46b0b5a77f5ce062b55b47c2ba
Irpear <145675108+Irpear@users.noreply.github.com>
2024-09-24 12:00:06 +0200

    db pagina

---
+++ css/db.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost'; // Of je eigen servernaam
+$db   = 'donaties';  // Naam van je database
+$user = 'root';      // Gebruikersnaam van je MySQL
+$pass = '';          // Wachtwoord voor je MySQL-gebruiker (kan leeg zijn op lokaal)
+
+?>
--- doneren.php
+++ doneren.php
@@ -1,3 +1,25 @@
+<?php
+require 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $naam = $_POST['name'];
+    $bedrag = $_POST['donation'];
+
+    $sql = "INSERT INTO donaties (naam, bedrag) VALUES (:naam, :bedrag)";
+    $stmt->execute(['naam' => $naam, 'bedrag' => $bedrag]);
+
+    header('Location: doneren.php');
+    exit();
+}
+
+
+$sql = "SELECT SUM(bedrag) AS totaal_bedrag FROM donaties";
+$stmt = $pdo->query($sql);
+$row = $stmt->fetch();
+$totaal_bedrag = $row['totaal_bedrag'];
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -35,7 +57,7 @@
 
                 <H1>Totaal gedoneerd bedrag</H1>
                 <img src="img\heartborder.png" hartje" width="100%">
-                <p>php berekend bedrag hier</p>
+                <p>€ <?php echo number_format($totaal_bedrag, 2); ?></p>
 
             </div>
         </div>
