837d7f29745b9724bc0784907fde995f57ba7ee4
Irpear <145675108+Irpear@users.noreply.github.com>
2024-09-24 11:37:25 +0200

    donatie skelet klaar

--- TLE1 WK2/doneren.php
+++ TLE1 WK2/doneren.php
@@ -12,26 +12,30 @@
     <nav> </nav>
     <main>
         <div class=pagediv>
-            <div>
+            <div class=divide1>
                 <H1>Hier kunt u doneren aan Tuvalu</H1>
 
                 <form action="">
-                    <div>
-                        <label for="name">Uw naam</label>
-                        <input type="text" id="name" name="name" placeholder="Naam" required>
-                    </div>
-                    <div>
-                        <label for="donation">Uw donatie</label>
-                        <input type="int" id="donation" name="donation" placeholder="€..." required>
-                    </div>
+                    <div class=formdiv>
+                        <div class=inputdiv>
+                            <label for="name">Uw naam</label>
+                            <input type="text" id="name" name="name" placeholder="Naam" required maxlength="20">
+                        </div>
+                        <div class=inputdiv>
+                            <label for="donation">Uw donatie</label>
+                            <input type="number" id="donation" name="donation" placeholder="€..." required min="0" step="0.01">
+                        </div>
 
-                    <button type="submit">Doneren</button>
+                        <button type="submit">Doneren</button>
+                    </div>
                 </form>
             </div>
 
-            <div>
+            <div class=divide2>
 
                 <H1>Totaal gedoneerd bedrag</H1>
+                <img src="img\heartborder.png" hartje" width="100%">
+                <p>php berekend bedrag hier</p>
 
             </div>
         </div>
